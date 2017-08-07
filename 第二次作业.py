6b6b44c0e3d4d91651fdf1558455c47d5bbf8457
赵宝 <zhaobao@lubanec.com>
2017-08-07 11:23:39 +0800

    1

--- 第二次作业.py
+++ 第二次作业.py
@@ -1,10 +1,11 @@
-a = input('请输入成绩：')
-if a.isdigit():
-    print('类型为：', type(a))
-    num=int(a)
-    print('转换为整数', num)
-    if num<=100:
-        if num>=90:
+a = input("请输入值:")
+print('类型为：', type(a))
+try:
+    num = float(a)
+    print(type(num))
+    print('转换为数值型', num)
+    if num <= 100:
+        if num >= 90:
             print('你的成绩为优秀')
         elif num>=80 and num<90:
             print('你的成绩为良好')
@@ -14,5 +15,5 @@ if a.isdigit():
             print('你的成绩为不合格')
     else:
         print('输入的值不能大于100，请重新输入！')
-else:
-    print('请输入纯数字!')
+except:
+    print('请输入数字')
