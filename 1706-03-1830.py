95bb02127797f8a046fd69a34574bd49f86f97d0
zhaobao1830 <x5219438>
2017-08-08 23:17:08 +0800

    1

---
+++ 1706-03-1830.py
@@ -0,0 +1,16 @@
+# 1.完成函数文档字符串的使用
+# def test1(x):
+#     return abs(x)
+
+# 3.name = 'tom'
+#    age = 20
+#    使用字符串格式化输出打印name和age
+
+# 输出name
+name = "name = '{0}'".format('tom')
+print(name)
+
+# 输出age
+def tt1():
+    return '%d'%(20)
+print("age="+tt1())
